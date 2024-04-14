41846eb1b361767debbfdf774943e570080c25e7
KartikPat250905 <kartikpat25@gmail.com>
2024-04-14 10:49:10 +0300

    Create helloworld.cpp

---
+++ C++ basics/helloworld.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    cout << "Hello world\n";
+    //cout << "Hello world" << endl;
+    //cout << "Hello world" << "\n";
+
+    int a = 123;
+    cout << a << endl;
+
+    char c = 'A';
+    cout << c << endl;
+
+    bool d = true;
+    cout << d << endl;
+
+    float  f = 1.25;
+    cout << f << endl;
+
+    int size = sizeof(a);
+    cout << "size of a is:" << size << endl;
+    */
+    
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 97;
+    cout<< ch << endl;
+}
